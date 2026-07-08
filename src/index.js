80cac0f75ec48e6d677f5bc7f8e6ba941a450a01
MAX208V <max208v@users.noreply.github.com>
2026-07-08 13:29:43 +0000

    fix: remove all custom headers from image download req

--- src/index.js
+++ src/index.js
@@ -37,11 +37,7 @@ gopeed.events.onResolve(async function(ctx) {
         files.push({
           name: username + '_' + shortcode + titlePart + '_' + pad2(imgIdx) + '.jpg',
           req: {
-            url: best.url,
-            headers: {
-              'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1',
-              'Referer': 'https://www.instagram.com/'
-            }
+            url: best.url
           }
         });
       }
